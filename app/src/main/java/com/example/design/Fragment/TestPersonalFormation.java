578f9c8d5d1788af34c76ca63dfddce80363ca16
reiAndroid <krajae265@gmail.com>
2023-01-26 17:39:06 +0100

    test

--- app/src/main/java/com/example/design/Fragment/TestPersonalFormation.java
+++ app/src/main/java/com/example/design/Fragment/TestPersonalFormation.java
@@ -1,13 +1,10 @@
 package com.example.design.Fragment;
 
 import android.os.Bundle;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.fragment.app.Fragment;
-
 import com.example.design.R;
 
 public class TestPersonalFormation extends Fragment {
