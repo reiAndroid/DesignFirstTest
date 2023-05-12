f61b7a4b43bed756f8c7335a3406d2fe7e5d1eb5
reiAndroid <krajae265@gmail.com>
2023-05-12 11:18:37 +0200

    test

---
+++ app/src/main/java/com/example/design/Activities/FirstScreen.java
@@ -0,0 +1,14 @@
+package com.example.design.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.design.R;
+import android.os.Bundle;
+
+public class FirstScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
